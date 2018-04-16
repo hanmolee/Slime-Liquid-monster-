0fd5f7d4c6e6fefad19036a3a85d424b5c112b89
hanmo <dlgksah@gmail.com>
2018-04-16 11:56:03 +0900

    - Permission Activity 추가 - LockScreenService 및 LockScreenViewSevice 구현 완료

--- app/src/main/java/hanmo/com/slime/lockscreen/service/LockScreenViewService.kt
+++ app/src/main/java/hanmo/com/slime/lockscreen/service/LockScreenViewService.kt
@@ -1,5 +1,6 @@
 package hanmo.com.slime.lockscreen.service
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -8,20 +9,30 @@ import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.os.Message
+import android.provider.Settings
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
 import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 import hanmo.com.slime.R
+import hanmo.com.slime.db.RealmHelper
 import hanmo.com.slime.lockscreen.LockscreenUtil
+import hanmo.com.slime.lockscreen.PermissionActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 /**
  * Created by hanmo on 2018. 4. 15..
  */
 class LockScreenViewService : Service() {
 
-    private val LOCK_OPEN_OFFSET_VALUE = 50
+    private val LOCK_OPEN_OFFSET_VALUE = 50f
     private var mContext : Context? = null
     private var mInflater: LayoutInflater? = null
     private var mLockscreenView: View? = null
@@ -31,6 +42,7 @@ class LockScreenViewService : Service() {
     private var mBackgroundInLayout: RelativeLayout? = null
     private var mBackgroundLockImageView: ImageView? = null
     private var mForgroundLayout: RelativeLayout? = null
+    private var mShimmerTextView: TextView? = null
     private var mStatusBackgruondDummyView: RelativeLayout? = null
     private var mStatusForgruondDummyView: RelativeLayout? = null
 
@@ -62,7 +74,7 @@ class LockScreenViewService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         mMainHandler = SendMessageHandler()
-        if (isLockScreenAble()) {
+        if (isLockScreenAble()!!) {
             mWindowManager?.let {
                 mLockscreenView?.apply {
                     it.removeView(this)
@@ -134,7 +146,210 @@ class LockScreenViewService : Service() {
     }
 
     private fun isLockScreenAble() : Boolean? {
+        return RealmHelper.instance.getLockScreenIsLock()
+    }
+
+    private fun attachLockScreenView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(mContext)) {
+                val permissionActivityIntent = Intent(mContext, PermissionActivity::class.java)
+                permissionActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(permissionActivityIntent)
+
+                LockscreenUtil.getInstance(mContext).getPermissionCheckSubject()
+                        ?.subscribe {
+                            addLockScreenView()
+                        }
+            } else {
+                addLockScreenView()
+            }
+        } else {
+            addLockScreenView()
+        }
+    }
+
+    //@SuppressLint("ClickableViewAccessibility")
+    private fun addLockScreenView() {
+        if (mWindowManager != null && mLockscreenView != null && mParams != null) {
+            mLockscreenView?.setOnTouchListener { v, event -> false }
+            mWindowManager?.addView(mLockscreenView, mParams)
+            settingLockView()
+        }
+    }
+
+    private fun dettachLockScreenView() : Boolean? {
+        return if (isAttachedToWindow()!! && mWindowManager != null && mLockscreenView != null) {
+            mWindowManager?.removeView(mLockscreenView)
+            mLockscreenView = null
+            mWindowManager = null
+            stopSelf(mServiceStartId)
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun isAttachedToWindow() : Boolean? {
+        return mLockscreenView?.isAttachedToWindow
+    }
+
+    private fun settingLockView() {
+
+        mLockscreenView?.let {
+
+            mBackgroundLayout = it.findViewById(R.id.lockscreen_background_layout)
+            mBackgroundInLayout = it.findViewById(R.id.lockscreen_background_in_layout)
+            mBackgroundLockImageView = it.findViewById(R.id.lockscreen_background_image)
+            mForgroundLayout = it.findViewById(R.id.lockscreen_forground_layout)
+            mShimmerTextView = it.findViewById(R.id.shimmer_tv)
+            mForgroundLayout?.setOnTouchListener(mViewTouchListener)
+
+            mStatusBackgruondDummyView = it.findViewById(R.id.lockscreen_background_status_dummy)
+            mStatusForgruondDummyView = it.findViewById(R.id.lockscreen_forground_status_dummy)
+            setBackGroundLockView()
+
+            val displayMetrics : DisplayMetrics? = mContext?.resources?.displayMetrics
+            mDeviceWidth = displayMetrics?.widthPixels!!
+            mDevideDeviceWidth = (mDeviceWidth / 2)
+            mBackgroundLockImageView?.x = (mDevideDeviceWidth * -1).toFloat()
+
+            //kitkat
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                val statusBarHeight = LockscreenUtil.getInstance(mContext).statusBarHeight
+                val forgroundParam = mStatusForgruondDummyView?.layoutParams
+                forgroundParam?.height = statusBarHeight
+                mStatusForgruondDummyView?.layoutParams = forgroundParam
+
+                val alpha = AlphaAnimation(0.5F, 0.5F)
+                alpha.duration = 0
+                alpha.fillAfter = true
+                mStatusForgruondDummyView?.startAnimation(alpha)
+
+                val backgroundParam = mStatusBackgruondDummyView?.layoutParams
+                backgroundParam?.height = statusBarHeight
+                mStatusBackgruondDummyView?.layoutParams = backgroundParam
+
+            }
+
+        }
 
     }
+
+    private fun setBackGroundLockView() {
+        if (mIsLockEnable) {
+            mBackgroundInLayout?.setBackgroundColor(resources.getColor(R.color.lock_background_color))
+            mBackgroundLockImageView?.visibility = View.VISIBLE
+        } else {
+            mBackgroundInLayout?.setBackgroundColor(resources.getColor(android.R.color.transparent))
+            mBackgroundLockImageView?.visibility = View.GONE
+        }
+    }
+
+    private fun changeBackgroundLockView(forgroundX : Float) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (forgroundX < mDeviceWidth) {
+                mBackgroundLockImageView?.background = resources.getDrawable(R.drawable.lock)
+            } else {
+                mBackgroundLockImageView?.background = resources.getDrawable(R.drawable.unlock)
+            }
+        } else {
+            if (forgroundX < mDeviceWidth) {
+                mBackgroundLockImageView?.setBackgroundDrawable(resources.getDrawable(R.drawable.lock))
+            } else {
+                mBackgroundLockImageView?.setBackgroundDrawable(resources.getDrawable(R.drawable.unlock))
+            }
+
+        }
+    }
+
+    private val mViewTouchListener = object : View.OnTouchListener {
+        private var firstTouchX = 0f
+        private var layoutPrevX = 0f
+        private var lastLayoutX = 0f
+        private var layoutInPrevX = 0f
+        private var isLockOpen = false
+        private var touchMoveX = 0
+        private val touchInMoveX = 0
+
+        override fun onTouch(v: View, event: MotionEvent): Boolean {
+
+            when (event.action and MotionEvent.ACTION_MASK) {
+                MotionEvent.ACTION_DOWN -> {// 0
+                    firstTouchX = event.x
+                    layoutPrevX = mForgroundLayout?.x!!
+                    layoutInPrevX = mBackgroundLockImageView?.x!!
+                    if (firstTouchX <= LOCK_OPEN_OFFSET_VALUE) {
+                        isLockOpen = true
+                    }
+                }
+                MotionEvent.ACTION_MOVE -> { // 2
+                    if (isLockOpen) {
+                        touchMoveX = (event.rawX - firstTouchX).toInt()
+                        if (mForgroundLayout?.x!! >= 0) {
+                            mForgroundLayout?.x = (layoutPrevX + touchMoveX).toInt().toFloat()
+                            mBackgroundLockImageView?.x = (layoutInPrevX + touchMoveX / 1.8).toInt().toFloat()
+                            mLastLayoutX = lastLayoutX
+                            mMainHandler?.sendEmptyMessage(0)
+                            if (mForgroundLayout?.x!! < 0) {
+                                mForgroundLayout?.x = 0f
+                            }
+                            lastLayoutX = mForgroundLayout?.x!!
+                        }
+                    } else {
+                        return false
+                    }
+                }
+                MotionEvent.ACTION_UP -> { // 1
+                    if (isLockOpen) {
+                        mForgroundLayout?.x = lastLayoutX
+                        mForgroundLayout?.y = 0f
+                        optimizeForground(lastLayoutX)
+                    }
+                    isLockOpen = false
+                    firstTouchX = 0f
+                    layoutPrevX = 0f
+                    layoutInPrevX = 0f
+                    touchMoveX = 0
+                    lastLayoutX = 0f
+                }
+                else -> {
+                }
+            }
+
+            return true
+        }
+    }
+
+    private fun optimizeForground(forgroundX: Float) {
+        if (forgroundX < mDevideDeviceWidth) {
+            var startPostion = 0
+            startPostion = mDevideDeviceWidth
+            while (startPostion >= 0) {
+                mForgroundLayout?.x = startPostion.toFloat()
+                startPostion--
+            }
+
+        } else {
+            val animation = TranslateAnimation(0f, mDevideDeviceWidth.toFloat(), 0f, 0f)
+            animation.duration = 300
+            animation.setAnimationListener(object : Animation.AnimationListener{
+                override fun onAnimationRepeat(animation: Animation?) {  }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    mForgroundLayout?.x = mDevideDeviceWidth.toFloat()
+                    mForgroundLayout?.y = 0f
+                    dettachLockScreenView()
+                }
+
+                override fun onAnimationStart(animation: Animation?) {  }
+
+            })
+
+            mForgroundLayout?.startAnimation(animation)
+
+        }
+    }
+
+
 
 }
